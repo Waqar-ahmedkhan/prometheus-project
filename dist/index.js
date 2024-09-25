0a814e257bd3f4cd5d14bc7ddd804f798f75d503
Waqar ahmed <waqarahmed44870@gmail.com>
2024-09-25 23:21:41 +0500

    prometheus and grafana

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const port = process.env.PORT || 3000;
+// Mock API data
+const products = [
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 },
+];
+// GET all products
+app.get("/products", (req, res) => {
+    res.json(products);
+});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+// Mock API data
+const products = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+  { id: 3, name: "Product 3", price: 300 },
+];
+
+// GET all products
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+ 
+
+
+app.listen(port, ()=> {
+  console.log(`Server running on port ${port}`);
+});
